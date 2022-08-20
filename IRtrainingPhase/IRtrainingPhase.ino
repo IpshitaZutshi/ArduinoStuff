2a4a4b0f0c08ea64da9adda4a182621fc6fb9ca5
IpshitaZutshi <48216973+IpshitaZutshi@users.noreply.github.com>
2022-08-20 19:28:02 -0400

    Latest versions of the cue task

---
+++ IRtrainingPhase/IRtrainingPhase.ino
@@ -0,0 +1,81 @@
+//For this, do we want the mice to just roam around and touch the left, right, and home IR or do we also want to train them so that
+//they know that poking the door IR will open the door?
+#include <Servo.h>
+Servo myServo;
+
+const int openDoor = -270;   
+
+const int doorLeft = 15;
+const int doorRight = 16;
+const int doorBase = 17;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+int phase = 2; //1 mean the water will come out of solenoid as long as IR bream is broken. phase 2 will mean the water will only come out for a specific amount of time and will eventually stop. Then there will be a cooldown
+
+const int IRbase = 30;
+const int IRleft = 32;
+const int IRright = 33;
+
+const int solLeft = 48;                                             
+const int solBase = 50;
+const int solRight = 52;
+
+
+void setup() {
+  pinMode(IRbase, INPUT);
+  pinMode(IRleft, INPUT);
+  pinMode(IRright, INPUT);
+  
+  pinMode(solLeft, OUTPUT);
+  pinMode(solRight, OUTPUT);
+  pinMode(solBase, OUTPUT);
+
+  servo1.attach(doorLeft);
+  servo2.attach(doorRight);
+  servo3.attach(doorBase);
+
+  Serial.begin(9600);
+  Serial.flush();
+}
+
+void loop() {
+  servo1.write(openDoor);
+  servo2.write(openDoor);
+  servo3.write(openDoor);
+  
+  if(phase == 1) {
+    if(digitalRead(IRbase) == HIGH) {
+     digitalWrite(solBase, HIGH); 
+    }else{
+     digitalWrite(solBase, LOW);   
+    }
+    if(digitalRead(IRleft) == HIGH) {
+     digitalWrite(solLeft, HIGH); 
+    }else{
+     digitalWrite(solLeft, LOW);   
+    }
+    if(digitalRead(IRright) == HIGH) {
+     digitalWrite(solRight, HIGH); 
+    }else{
+     digitalWrite(solRight, LOW);   
+    }
+  }
+  if(phase == 2) {
+    if(digitalRead(IRbase) == HIGH) {
+    digitalWrite(solBase, HIGH); delay(50); digitalWrite(solBase, LOW);              //water will be given for 0.5 seconds and then stop. Cooldown is 6 seconds
+    delay(1000);
+    }
+    if(digitalRead(IRleft) == HIGH) {
+    digitalWrite(solLeft, HIGH); delay(50); digitalWrite(solLeft, LOW);
+    delay(1000);
+    }
+    if(digitalRead(IRright) == HIGH) {
+    digitalWrite(solRight, HIGH); delay(50); digitalWrite(solRight, LOW);
+    delay(1000);
+    }
+  }
+
+}
---
+++ MouseTrack/MouseTrack.ino
@@ -0,0 +1,568 @@
+      
+#include <Servo.h>
+Servo myServo;
+
+int state = 0;
+
+int phase = 3; //if phase = 1 (training), one door open + extended cue 
+               //if phase = 2 (training), both doors open + extended cue
+               //if phase = 3 (training), both doors open + variable length cue
+               //if phase = 4 HAS DELAY SO WE WILL NOW STICK WITH PHASE 3 FOR EXPERIMENT AS WELL
+               
+int optPhase = 0;   //0 = no stim
+                    //1 = during cue only
+                    //2 = during delay only
+                    //3 = cue + delay (the 5 seconds)
+                    //4 = delay + going to right/left nosepoke 
+
+int blocksSize = 11;//n+1                    
+                    
+int modality = 4; // make it "int modality = PUT IN A NUMBER (1,2,3,4) MANUALLY DEPENDING ON WHAT YOU WANT TO DO". For example, if u want to train all mice to do just LED manually enter "modality = 1"
+
+int biasCorrect = 0; //make it 1 or 0, if 1, the probability of a left trial is the fraction of the last 20 trials gone right
+const int numBiasTrials = 10;
+
+const int numRandTrials = 5;// variable to make sure that the left and right trials are equally presented
+
+int pos = 0;    
+
+int prevTrialCount = 0;  
+int countTrials = 0;
+int correctTrials = 0;
+int numLeft = 0;
+int numRight = 0;
+
+//------------ALL DELAYS---------------------------//                                 
+int cueDelay = 1000; // duration of cue           
+int doorBaseDelay = 2000;      //DEFAULT IS 2000        //cueDelay and doorbasedelay are no longer additive       
+int varyCue = 1;// if 0, cueDelay is constant, if 1 cueDelay changes;
+int cueDelayTimes[4] = {50,200,500,1000};
+                              
+int speakerDelay = 1000; // time for wrong tone
+int solenoidDelayL = 40;
+int solenoidDelay = 40;
+
+//-------------------------------------------------//
+
+const int openDoor = -270;                                                   
+const int closeDoor = -openDoor/3;
+
+
+bool doorLeftState = false;
+bool doorRightState = false;                                                  
+bool doorBaseState = false;                                                   
+                                                                              
+const int ledLeft = 4;
+const int ledRight = 5;
+
+const int speakerLeft = 20;                                         
+const int speakerRight = 21;                                        
+                                                                                                                
+const int solLeft = 48;                                             
+const int solBase = 50;
+const int solRight = 52;
+
+const int doorLeft = 15;
+const int doorRight = 16;
+const int doorBase = 17;
+
+const int IRbase = 30;
+const int IRdoor = 31;
+const int IRleft = 32;
+const int IRright = 33;
+
+int blocksCount = 1;
+boolean stimTrial = false;
+
+
+//INTAN PINS ========================================= 
+ 
+const int leftCue = 37;      //intan pin 6
+const int rightCue = 39;     //intan pin 7
+const int solPin = 41;       //intan pin 8   
+const int IRbasepin = 43;    //intan pin 9
+const int IRdoorpin = 45;    //intan pin 10
+const int IRleftpin = 47;    //intan pin 11
+const int IRrightpin = 49;   //intan pin 12
+const int delayIntan = 51;   //intan pin 13           //it will turn high when mouse pokes nose at doorIR. It will turn off when door opens. We can just subtract the duration of light from this to figure out how long the delay (period where there is no cue) was for the trials. IMPLEMENT THIS LATER
+const int optStim = 53;      //intan pin 14
+
+int startMillis;
+long cueMillis = 0;
+long delayMillis = 0;
+int cueON = 0;
+int delayON = 0;
+int wrongTrial = 0;
+int pastchoices[numBiasTrials];
+int newchoices[numBiasTrials];
+float bias = -1;
+float sumchoices = -1;
+
+int pastTrials[numRandTrials];
+int newTrials[numRandTrials];
+float trialbias = -1;
+float trialsumchoices = -1;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+long randNumber = 0;
+long randNumberModality;     
+
+void setup() {
+
+  //OUTPUTS 
+  pinMode(ledLeft, OUTPUT);
+  pinMode(ledRight, OUTPUT);
+  pinMode(speakerLeft, OUTPUT);
+  pinMode(speakerRight, OUTPUT);
+  pinMode(solLeft, OUTPUT);
+  pinMode(solRight, OUTPUT);
+  pinMode(solBase, OUTPUT);
+
+  pinMode(leftCue, OUTPUT); digitalWrite(leftCue, LOW);
+  pinMode(rightCue, OUTPUT); digitalWrite(rightCue, LOW);
+  pinMode(solPin, OUTPUT); digitalWrite(solPin, LOW);
+  pinMode(IRbasepin, OUTPUT); digitalWrite(IRbasepin, LOW);
+  pinMode(IRdoorpin, OUTPUT); digitalWrite(IRdoorpin, LOW);
+  pinMode(IRleftpin, OUTPUT); digitalWrite(IRleftpin, LOW);
+  pinMode(IRrightpin, OUTPUT); digitalWrite(IRrightpin, LOW);
+  pinMode(delayIntan, OUTPUT); digitalWrite(delayIntan, LOW);      
+
+  pinMode(optStim, OUTPUT); digitalWrite(optStim, LOW);
+  
+
+  servo1.attach(doorLeft);
+  servo2.attach(doorRight);
+  servo3.attach(doorBase);
+
+  //INPUTS
+  pinMode(IRbase, INPUT);
+  pinMode(IRdoor, INPUT);
+  pinMode(IRleft, INPUT);
+  pinMode(IRright, INPUT);
+  
+  Serial.begin(9600);
+  Serial.flush();
+  startMillis  = millis();
+
+  randomSeed(analogRead(0));
+}
+
+void loop(){
+  
+  if(state == 0) {                                             //state = 0 means home base
+    if(digitalRead(IRdoor) == HIGH) {
+      if(blocksCount == blocksSize) {
+        stimTrial = !stimTrial;
+        blocksCount = 1;
+      }      
+          
+      delayMillis = millis();
+      delayON = 1;      
+      digitalWrite(delayIntan, HIGH);    
+       
+      if (varyCue==1){        
+        cueDelay = cueDelayTimes[random(0,4)];
+      }
+      
+      if (biasCorrect==0){ // Don't correct for mouse's bias, but make sure that the arduino doesn't generate way more trials towards a certain side
+         if (countTrials<numRandTrials){  
+            randNumber = random(0,2);           
+          } else{
+              trialsumchoices = 0;
+              for (int ii = 0; ii<numRandTrials; ii++){
+                trialsumchoices += newTrials[ii]; 
+              }
+              trialbias = round((trialsumchoices/numRandTrials)*100);
+              randNumber = 1;//randNumberProb(trialbias); //make it so that randNumber is 0 or 1 with the probability of past trials;                     
+         }
+         for (int ii = 0; ii<(numRandTrials-1); ii++){
+           newTrials[ii+1] = pastTrials[ii]; 
+         }
+         memcpy(pastTrials,newTrials, sizeof(newTrials));
+         newTrials[0] = randNumber;
+              
+      } else if (biasCorrect==1){
+          if (countTrials<numBiasTrials){  
+            randNumber = random(0,2);
+            
+          } else{
+              sumchoices = 0;
+              for (int ii = 0; ii<numBiasTrials; ii++){
+                sumchoices += newchoices[ii]; 
+              }
+              bias = round((sumchoices/numBiasTrials)*100);
+              randNumber = randNumberProb(bias); //make it so that randNumber is 0 or 1 with the probability of pastchoices;  
+         
+          }
+          for (int ii = 0; ii<(numBiasTrials-1); ii++){
+            newchoices[ii+1] = pastchoices[ii]; 
+          }
+          memcpy(pastchoices,newchoices, sizeof(newchoices));
+      }
+      //digitalWrite(IRdoorpin, HIGH); delay(1); digitalWrite(IRdoorpin, LOW);
+      
+      if(randNumber == 0) {
+        numLeft = numLeft+1;
+        if(modality == 1) {
+          digitalWrite(ledLeft, HIGH); 
+          digitalWrite(leftCue, HIGH);  
+          if (phase == 3){
+            cueMillis = millis();
+            cueON=1;
+          }
+          else if (phase == 4) {
+            delay(cueDelay); 
+            digitalWrite(ledLeft, LOW);
+            digitalWrite(leftCue, LOW);
+          }
+        }
+        else if(modality == 2) {
+          tone(speakerLeft, 10000);
+          digitalWrite(leftCue, HIGH);
+          if (phase == 3){
+            cueMillis = millis();
+            cueON=1;
+          }
+          else if (phase == 4) {
+            delay(cueDelay);
+            noTone(speakerLeft);
+            digitalWrite(leftCue, LOW);
+          }          
+        }
+        else if(modality == 3) {
+          randNumberModality = random(0,2);
+          if(randNumberModality == 0) {                                                   //if randNumberModality is 0 --> LED will be the presented cue. If randNumberModality is 1 --> speaker will be the presented cue
+            digitalWrite(ledLeft, HIGH); 
+            digitalWrite(leftCue, HIGH);  
+            if (phase == 3){
+              cueMillis = millis();
+              cueON=1;
+            }
+            else if (phase == 4) {
+              delay(cueDelay); 
+              digitalWrite(ledLeft, LOW);
+              digitalWrite(leftCue, LOW);
+            }
+          }
+          else if(randNumberModality == 1) {
+            tone(speakerLeft, 10000);
+            digitalWrite(leftCue, HIGH);
+            if (phase == 3){
+              cueMillis = millis();
+              cueON=1;
+            }
+            else if (phase == 4) {
+              delay(cueDelay);
+              noTone(speakerLeft);
+              digitalWrite(leftCue, LOW);
+            } 
+          }
+        }
+        else if(modality == 4) {
+          digitalWrite(ledLeft, HIGH); 
+          digitalWrite(leftCue, HIGH);  
+          tone(speakerLeft, 10000);
+          if (phase == 3){
+            cueMillis = millis();
+            cueON=1;
+            if(optPhase == 1 || optPhase == 3) {                   //opto turns on for cue phase or cue + delay phase
+              digitalWrite(optStim, stimTrial);
+            } else {digitalWrite(optStim, LOW); 
+          }
+          }
+          else if (phase == 4) {
+            delay(cueDelay);
+            noTone(speakerLeft);
+            digitalWrite(leftCue, LOW);
+            digitalWrite(ledLeft, LOW);        
+          }          
+                
+        if(phase == 1) {            //in training, only left door (correct door) opens
+          servo1.write(openDoor);
+          doorLeftState = true;
+        } 
+        else {       //in experiment, both doors open
+          servo1.write(openDoor);
+          doorLeftState = true;
+          servo2.write(openDoor);
+          doorRightState = true;
+        }
+       }
+      } else if(randNumber == 1) {
+        numRight = numRight+1;
+        if(modality == 1) {
+          digitalWrite(ledRight, HIGH); 
+          digitalWrite(rightCue, HIGH); 
+          if (phase == 3){
+            cueMillis = millis();
+            cueON=1;
+          }
+          else if (phase == 4) {
+            delay(cueDelay); 
+            digitalWrite(ledRight, LOW);
+            digitalWrite(rightCue, LOW);
+          }          
+        }
+        else if(modality == 2) {
+          tone(speakerRight, 10000);
+          digitalWrite(rightCue, HIGH);
+          if (phase == 3){
+            cueMillis = millis();
+            cueON=1;
+          }
+          else if (phase == 4) {
+            delay(cueDelay);
+            noTone(speakerRight);
+            digitalWrite(rightCue, LOW);
+          }  
+        }
+        else if(modality == 3) {
+          randNumberModality = random(0,2);
+          if(randNumberModality == 0) {
+            digitalWrite(ledRight, HIGH); 
+            digitalWrite(rightCue, HIGH); 
+            if (phase == 3){
+              cueMillis = millis();
+              cueON=1;
+            }
+            else if (phase == 4) {
+              delay(cueDelay); 
+              digitalWrite(ledRight, LOW);
+              digitalWrite(rightCue, LOW);
+            }  
+          }
+          else if(randNumberModality == 1) {
+            tone(speakerRight, 10000);
+            digitalWrite(rightCue, HIGH);
+            if (phase == 3){
+              cueMillis = millis();
+              cueON=1;
+            }
+            else if (phase == 4) {
+              delay(cueDelay);
+              noTone(speakerRight);
+              digitalWrite(rightCue, LOW);
+            }  
+          }
+        }
+        else if(modality == 4) {
+          digitalWrite(ledRight, HIGH); 
+          digitalWrite(rightCue, HIGH);        
+          tone(speakerRight, 10000);
+          if (phase == 3){
+            cueMillis = millis();
+            cueON=1;
+            if(optPhase == 1 || optPhase == 3) {                   //opto turns on for cue phase or cue + delay phase
+              digitalWrite(optStim, stimTrial);
+            } else {digitalWrite(optStim, LOW); 
+          }
+          }
+          else if (phase == 4) {
+            delay(cueDelay);
+            noTone(speakerRight);
+            digitalWrite(rightCue, LOW);
+            digitalWrite(ledRight, LOW);         
+          }          
+        }  
+        if(phase == 1) {
+          servo2.write(openDoor);
+          doorRightState = true;
+        }
+        else {
+          servo2.write(openDoor);
+          doorRightState = true;
+          servo1.write(openDoor);
+          doorLeftState = true;
+        }     
+      }
+      state = 1;
+    }
+  }
+  else if(state == 1) {            //mouse is on its way to correct side (right or left)
+    if(randNumber == 0) {
+      if(digitalRead(IRleft) == HIGH) {
+        if(optPhase == 4) {                   //opto turns off for delay + right/left nosepoke phase
+          digitalWrite(optStim, LOW);
+        }
+      //  digitalWrite(IRleftpin, HIGH); delay(10); digitalWrite(IRleftpin, LOW);
+        digitalWrite(solLeft, HIGH); digitalWrite(solPin, HIGH); delay(solenoidDelayL); digitalWrite(solLeft, LOW); digitalWrite(solPin, LOW);
+        digitalWrite(ledLeft, LOW);
+        digitalWrite(leftCue, LOW);        
+        noTone(speakerLeft);      
+        
+        if(doorRightState) {
+        servo2.write(closeDoor);
+        }
+        doorRightState = false;
+        state = 2;
+         //INCREMENT CORRECT TRIAL
+        correctTrials= correctTrials+1;
+        newchoices[0] = 0;
+        wrongTrial = 0;
+      }
+      else if(digitalRead(IRright) == HIGH) {
+        if(optPhase == 4) {                   //opto turns off for delay + right/left nosepoke phase
+          digitalWrite(optStim, LOW);
+        }
+      //  digitalWrite(IRrightpin, HIGH); delay(10); digitalWrite(IRrightpin, LOW);
+        digitalWrite(ledLeft, LOW);
+        digitalWrite(leftCue, LOW);
+        noTone(speakerLeft); 
+
+        if(doorLeftState) {                                             
+        servo1.write(closeDoor);
+        }
+        doorLeftState = false;          
+        tone(speakerLeft, 3000); tone(speakerRight, 3000);                            //if they get it wrong, both speakers play a sound
+        delay(speakerDelay);
+        noTone(speakerLeft); noTone(speakerRight); 
+        state = 2;
+        newchoices[0] = 1;
+        //DON'T INCREMENT CORRECT TIRAL BECAUSE ITS WRONG
+        wrongTrial = 1;
+      }
+    }
+    else if(randNumber == 1) {
+      if(digitalRead(IRright) == HIGH) {
+        if(optPhase == 4) {                   //opto turns off for delay + right/left nosepoke phase
+          digitalWrite(optStim, LOW);
+        }
+      //  digitalWrite(IRrightpin, HIGH); delay(10); digitalWrite(IRrightpin, LOW);
+        digitalWrite(solRight, HIGH); digitalWrite(solPin, HIGH); delay(solenoidDelay); digitalWrite(solRight, LOW); digitalWrite(solPin, LOW);
+        digitalWrite(ledRight, LOW); 
+        digitalWrite(rightCue, LOW);       
+        noTone(speakerRight);        
+
+        if(doorLeftState) {                                             
+          servo1.write(closeDoor);
+        }
+        doorLeftState = false;
+        state = 2;
+        wrongTrial = 0;
+        newchoices[0] = 1;
+        //INCREMENT CORRECT TRIAL
+        correctTrials= correctTrials+1;
+      }
+      else if(digitalRead(IRleft) == HIGH) {
+        if(optPhase == 4) {                   //opto turns off for delay + right/left nosepoke phase
+          digitalWrite(optStim, LOW);
+        }
+      //  digitalWrite(IRleftpin, HIGH); delay(10); digitalWrite(IRleftpin, LOW); 
+        digitalWrite(ledRight, LOW); 
+        digitalWrite(rightCue, LOW); 
+        noTone(speakerRight); 
+
+        if(doorRightState) {
+          servo2.write(closeDoor);
+        }
+        doorRightState = false;         
+        tone(speakerLeft, 3000); tone(speakerRight, 3000);                            //if they get it wrong, both speakers play a sound
+        delay(speakerDelay);
+        noTone(speakerLeft); noTone(speakerRight);
+        state = 2;
+        wrongTrial = 1;
+        newchoices[0] = 0;
+        //DON'T INCREMENT CORRECT TIRAL BECAUSE ITS WRONG
+      }
+    }
+  }
+  else if(state == 2) {    //mouse on its way back to home base but trial hasn't resetted yet
+    
+    if(digitalRead(IRbase) == HIGH) {
+      //if (wrongTrial == 0){
+        digitalWrite(solBase, HIGH); digitalWrite(solPin, HIGH); 
+        delay(solenoidDelay); 
+        digitalWrite(solBase, LOW);digitalWrite(solPin, LOW);
+      //}
+      if(doorLeftState) {                                             
+        servo1.write(closeDoor);
+      }
+      if(doorRightState) {
+        servo2.write(closeDoor);
+      }
+      
+      if(doorBaseState) {          
+        servo3.write(closeDoor);
+      }
+      
+      doorLeftState = false;
+      doorRightState = false;
+      doorBaseState = false;
+      
+      state = 0; //state goes back to 0 and trial resets
+      countTrials = countTrials+1;
+      blocksCount = blocksCount+1;
+    }
+         
+  }
+
+  if (phase==3){
+    if (cueON==1){
+      if (millis()-cueMillis>cueDelay){      
+        digitalWrite(ledLeft, LOW);
+        digitalWrite(leftCue, LOW);
+        noTone(speakerLeft);       
+        digitalWrite(ledRight, LOW); 
+        digitalWrite(rightCue, LOW); 
+        noTone(speakerRight);       
+        cueON=0;
+        if(optPhase == 1) {
+          digitalWrite(optStim, LOW);             //opto turns off for cue phase
+        }
+        if(optPhase == 2 || optPhase == 4) {
+          digitalWrite(optStim, stimTrial);       //opto turns on for delay phase or delay + right/left nosepoke phase
+        }
+    }
+   }
+  }
+
+  if (delayON==1){
+   if (millis()-delayMillis>doorBaseDelay){   
+     servo3.write(openDoor);
+     doorBaseState = true;
+     digitalWrite(delayIntan, LOW);
+     delayON=0;
+     if(optPhase == 2 || optPhase == 3) {
+        digitalWrite(optStim, LOW);          //opto turns off for delay phase or cue + delay phase
+     }              
+   }
+  }
+ 
+  digitalWrite(IRbasepin,digitalRead(IRbase));
+  digitalWrite(IRrightpin,digitalRead(IRright));
+  digitalWrite(IRleftpin,digitalRead(IRleft));
+  digitalWrite(IRdoorpin,digitalRead(IRdoor));
+
+  Serial.print(millis()-startMillis);
+  Serial.print("ms, modality: ");
+  Serial.print(modality);
+  Serial.print(", arm: ");
+  Serial.print(randNumber);  
+  Serial.print(", total trials: ");
+  Serial.print(countTrials);    
+  Serial.print(", correct trials: ");
+  Serial.print(correctTrials);
+  Serial.print(", left: ");
+  Serial.print(numLeft);
+  Serial.print(", right: ");
+  Serial.print(numRight); 
+  Serial.print(", bias: ");
+  Serial.print(bias);   
+  Serial.print(", delay: ");
+  Serial.print(cueDelay);  
+  Serial.print('\n');
+ 
+}
+
+int randNumberProb(int x){
+  int result;
+  result = random(0,100);
+  if (result<(x-1)){
+    return 0;
+  }else{
+    return 1;
+  }
+}
