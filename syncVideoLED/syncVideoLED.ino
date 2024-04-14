bf7925e6a4cdb362543139f32f56e9b7dc3bc250
IpshitaZutshi <48216973+IpshitaZutshi@users.noreply.github.com>
2024-04-14 19:03:48 -0400

    changes on sleep box

--- syncVideoLED/syncVideoLED.ino
+++ syncVideoLED/syncVideoLED.ino
@@ -1,6 +1,7 @@
 // IO Pins definitions
 int intanPin1 = 2;
 int intanPin2 = 3;
+int Npx = 4;
 int ledPin1 = 8;
 int ledPin2 = 9;
 int pulseDur = 100; //duration of pulse on, in ms
@@ -8,25 +9,28 @@ int latencyDur[4] = {500,1500,700,2000};
 
 
 void setup() {
+  Serial.begin(57600);
   pinMode(intanPin1, OUTPUT); //define both IO pins as outputs
   pinMode(intanPin2, OUTPUT);
+  pinMode(Npx, OUTPUT);
   pinMode(ledPin1, OUTPUT); //define both IO pins as outputs
   pinMode(ledPin2, OUTPUT);  
-  Serial.begin(57600);
   randomSeed(analogRead(0));
-    while (Serial.read() != 115) {
+  while (Serial.read() != 115) {
    }
 }
 
 void loop() {
 
     digitalWrite(intanPin1, HIGH);  
-    digitalWrite(intanPin2, HIGH);  
+    digitalWrite(intanPin2, HIGH); 
+    digitalWrite(Npx, HIGH); 
     digitalWrite(ledPin1, HIGH); 
     digitalWrite(ledPin2, HIGH);  
     delay(pulseDur);
     digitalWrite(intanPin1, LOW);  
     digitalWrite(intanPin2, LOW); 
+    digitalWrite(Npx, LOW); 
     digitalWrite(ledPin1, LOW); 
     digitalWrite(ledPin2, LOW); 
 
