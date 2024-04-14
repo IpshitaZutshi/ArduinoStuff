d05616c42486a1b7b979147f88016b0512027d4b
IpshitaZutshi <48216973+IpshitaZutshi@users.noreply.github.com>
2024-04-14 19:06:15 -0400

    Update syncVideoLED.ino

--- syncVideoLED/syncVideoLED.ino
+++ syncVideoLED/syncVideoLED.ino
@@ -6,6 +6,7 @@ int ledPin1 = 8;
 int ledPin2 = 9;
 int pulseDur = 100; //duration of pulse on, in ms
 int latencyDur[4] = {500,1500,700,2000};
+int goSig = 0;
 
 
 void setup() {
@@ -16,12 +17,18 @@ void setup() {
   pinMode(ledPin1, OUTPUT); //define both IO pins as outputs
   pinMode(ledPin2, OUTPUT);  
   randomSeed(analogRead(0));
-  while (Serial.read() != 115) {
-   }
 }
 
 void loop() {
 
+  if (Serial.available() > 0) {          
+    goSig = 1;  
+  } else {
+    goSig = 0;
+    return;
+  }
+
+  if (goSig == 1){
     digitalWrite(intanPin1, HIGH);  
     digitalWrite(intanPin2, HIGH); 
     digitalWrite(Npx, HIGH); 
@@ -37,4 +44,5 @@ void loop() {
     //Random delay between blinks
     int curDelay = random(0,4);
     delay(latencyDur[curDelay]);
+  }
 }
